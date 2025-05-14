f0ca31c494b83231929ffa0970a11c57d6faa39e
valenciusap17 <v.apriady@gmail.com>
2025-05-15 04:33:57 +0700

    fix: bugs on seed used

--- authentication/src/seedUser.ts
+++ authentication/src/seedUser.ts
@@ -1,28 +1,38 @@
-import { NewUser } from "@db/schema/users";
-import { registerService } from "./user/services";
+import { NewUser, users } from "@db/schema/users";
+import bcrypt from "bcryptjs";
+import { db } from "./db";
 
 const DUMMY_USER_ID = process.env.DUMMY_USER_ID;
 if (!DUMMY_USER_ID) {
-  throw new Error("TENANT_ID or DUMMY_USER_ID or DUMMY_PRODUCT_ID not found");
+  throw new Error("DUMMY_USER_ID not found");
+}
+const SERVER_TENANT_ID = process.env.TENANT_ID;
+if (!SERVER_TENANT_ID) {
+  throw new Error("TENANT_ID not found");
 }
 
 async function generateUser() {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash("seedingpassword", salt);
+
   const newUser: NewUser = {
     id: DUMMY_USER_ID,
     email: "seed@gmail.com",
-    password: "seedingpassword",
+    password: hashedPassword,
     username: "seedinguser",
+    tenant_id: SERVER_TENANT_ID,
   };
 
-  registerService(
-    newUser.username,
-    newUser.email,
-    newUser.password,
-    "",
-    "",
-    ""
-  );
-  // await db.insert(users).values(newUser);
+  // registerService(
+  //   newUser.id!,
+  //   newUser.username,
+  //   newUser.email,
+  //   newUser.password,
+  //   "",
+  //   "",
+  //   ""
+  // );
+  await db.insert(users).values(newUser);
 }
 
 async function main() {

2c43784c9e9f19dd7b01bf362060a579ee8c4bdc
ferryhieronimus <ferryhieronimus@gmail.com>
2025-05-15 15:28:36 +0700

    chore: update Kubernetes configuration files for consistency

--- products/src/product/validation/getAllProduct.schema.ts
+++ products/src/product/validation/getAllProduct.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const getAllProductSchema = z.object({
   query: z.object({
-    page_number: z.coerce.number(),
-    page_size: z.coerce.number(),
+    page_number: z.coerce.number().min(1).max(1000),
+    page_size: z.coerce.number().min(1).max(100),
   }),
 });
